1896a93a826743714795d460c9f16a16506d319c
Ritesh Firodiya <ritesh@Riteshs-MacBook-Pro.local>
2022-06-03 16:10:44 +0530

    changed added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Hello: Build 1: Vercel</div>;
+  return (
+    <div>
+      Hello: Build 1:
+      <h1>Heroku</h1>
+    </div>
+  );
 };
 
 export default Home;
